86b212b520d742353b31283ee08f07f0f259ca57
ranedezkaertech <ranedez@kaertech.com>
2026-06-10 14:19:55 +0800

    changes in port

--- app.py
+++ app.py
@@ -477,4 +477,4 @@ if __name__ == '__main__':
         ensure_indexes()
     except Exception:
         pass
-    app.run(host='0.0.0.0', port=5001, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=5050, debug=True, use_reloader=False)
